bcf664acee6c5f47a037edca0deb4b9c86646d04
alK13 <webpetit@gmail.com>
2014-05-27 14:03:09 +0200

    Replace testing paremeters and implement Routing methods of the FOSJsBundle

--- Resources/public/js/crud.js
+++ Resources/public/js/crud.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $('.crud-delete').click(function() {
+    $('.crud-entity-delete').click(function() {
         $('#entity-delete .btn-danger').attr('data-id', $(this).attr('data-id'));
         $('#entity-delete .btn-danger').attr('data-entity', $(this).attr('data-entity'));
         $('#entity-delete').modal();
@@ -8,7 +8,7 @@ $(document).ready(function() {
     $('#entity-delete .btn-danger').click(function() {
         $.ajax({
             type: 'DELETE',
-            url: 'http://symfony.debian/app_dev.php/crud-delete/' + $(this).attr('data-entity') + '/' + $(this).attr('data-id')
+            url:  Routing.generate('developathe_crud_delete', {'entity': $(this).attr('data-entity'), 'id': $(this).attr('data-id')})
         }).done(function(){
             location.reload();
         });
