8f7c32f4868225f95bccb995b924fc324ac8a75c
alK13 <webpetit@gmail.com>
2014-06-06 19:58:42 +0200

    Miscellaneous changes: - Remove the test for the controller API, - Implement inline bootstrap class, - Copy the base and the pagination templates into the Bundle within the CRUD entity.

--- Generator/DevelopatheCrudGenerator.php
+++ Generator/DevelopatheCrudGenerator.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Developathe\CrudBundle\Generator;
+
 use Sensio\Bundle\GeneratorBundle\Generator\DoctrineCrudGenerator;
 use Symfony\Component\HttpKernel\Bundle\BundleInterface;
 use Doctrine\ORM\Mapping\ClassMetadataInfo;
@@ -18,12 +19,26 @@ class DevelopatheCrudGenerator extends DoctrineCrudGenerator
         parent::generate($bundle, $entity, $metadata, $format, $routePrefix, $needWriteActions, $forceOverwrite);
 
         $dir = sprintf('%s/Resources/views/%s', $this->bundle->getPath(), str_replace('\\', '/', $this->entity));
-        
+
+        $this->copyBasicTemplates();
         
         $this->generateCommonView($dir);
-        
+
         if (in_array('delete', $this->actions)) {
             $this->generateDeleteView($dir);
+        }
+    }
+
+    protected function copyBasicTemplates()
+    {
+        $dir = sprintf('%s/Resources/views', $this->bundle->getPath());
+        $base = "$dir/developathe.crud.base.html.twig";
+        if (!$this->filesystem->exists($base)) {
+            $this->filesystem->copy(__DIR__. '/../Resources/views/base.html.twig', $base);
+        }
+        $pagination = "$dir/developathe.crud.pagination.html.twig";
+        if (!$this->filesystem->exists($pagination)) {
+            $this->filesystem->copy(__DIR__. '/../Resources/views/pagination.html.twig', $pagination);
         }
     }
 
@@ -42,6 +57,7 @@ class DevelopatheCrudGenerator extends DoctrineCrudGenerator
             'actions'           => $this->actions,
         ));
     }
+
     /**
      * Generates the Common.html.twig template in the final bundle.
      *
@@ -57,5 +73,5 @@ class DevelopatheCrudGenerator extends DoctrineCrudGenerator
             'actions'           => $this->actions,
         ));
     }
-    
+
 }
